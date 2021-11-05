ff238a2fd4a044b0468c264cee389c1b98acd7f1
joshasgard <idowuodesanmi@gmail.com>
2021-11-05 18:46:53 +0100

    Updated ETL process_data.py Script

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,79 @@
 import sys
-
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """Function to extract raw .csv data files containing messages and 
+       their categories, and then merge them. 
+    
+    Args: 
+        messages_filepath(str): path to raw .csv message file.
+        categories_filepath(str): path to raw .csv message categories file.
+
+    Returns:
+        dataframe: containing messages and respective categories ready for cleaning.
+    
+    """
+    #   Import data files
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    #   Merge datasets
+    df = pd.merge(messages, categories)
+    
+    return df
 
 
 def clean_data(df):
-    pass
+    """Function to transform dataset for use in an ML Pipeline.
+       Splits the categories into separate category columns, cleans class values
+       for each category, removes duplicates. 
+       
+    Args:
+        df: dataframe to be cleaned.
+
+    Returns:
+        dataframe(df): ML-ready dataset.
+ 
+    """
+    #   Split the categories column into separate columns 
+    categories_table = df.categories.str.split(';',expand = True)
+    categories_table.columns = categories_table.iloc[0].apply(lambda x: x[0:-2]) 
+    
+    #   Clean class values for each category and cast as numeric
+    for column in categories_table:
+        categories_table[column] = categories_table[column].str[-1].astype('int')
+
+    #   Replace 'categories' column in df with new columns
+    df.drop('categories', axis = 1, inplace=True)
+    df = pd.concat([df,categories_table], axis=1)
+
+    #   Remove dupplicates from data
+    df = df.drop_duplicates(subset='message')
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """Function to create SQL database engine and to load transformed data into 
+       the database. 
+    
+    Args: 
+        dataframe(df): transformed dataset.
+        database_filename(str): SQL database engine name. 
+
+    Returns:
+        None
+    
+    """
+    #   Create SQL engine with specified filename
+    engine = create_engine(database_filename)
+
+    #   Load cleaned data into SQL engine, replacing data in database if defined 
+    #   name already exists.
+    df.to_sql('cleaned_messages', engine, index = False, if_exists = 'replace')
+      
 
 
 def main():
