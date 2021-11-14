cb6b79c435a7048873e64fb6578f05b90dbf907a
joshasgard <idowuodesanmi@gmail.com>
2021-11-14 03:20:09 +0100

    Added a second chart

--- app/run.py
+++ app/run.py
@@ -40,35 +40,80 @@ def tokenize(text):
 
     return clean_tokens
 
-# load data
+#   load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('DisasterResponse', engine)
 
-# load model
+#   load model
 model = joblib.load("../models/classifier.pkl")
 
 
-# index webpage displays cool visuals and receives user input text for model
+#   index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
     
-    # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
+    #   Figure 1: data showing top 5 message categories in the dataset
+    genre_per_category = df.iloc[:,3:].groupby('genre').sum().T
+    top_category = genre_per_category.sum(axis=1).sort_values(ascending=False).reset_index()
+    top_category.columns = ['categories', 'true_proportion -1']
+    top_category['false_proportion -0'] = df.shape[0] - top_category['true_proportion -1']
+    top_category['categories'] = top_category['categories'].apply(lambda x: str(x).replace('_', ' '))
+    top_classes = top_category.head(5)
+
+    #   Figure 2: data visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
-    # create visuals
-    # TODO: Below is an example - modify to create your own visuals
+
+    #   Figure 3: 
+
+    #   create visuals
     graphs = [
         {
+            'data': [
+                Bar(
+                    name = 'Classified in class',
+                    y=top_classes['categories'],
+                    x=top_classes['true_proportion -1'],
+                    orientation = 'h',
+                    marker=dict(
+                            color='rgba(246, 78, 139, 0.6)',
+                            line=dict(color='rgba(246, 78, 139, 1.0)', width=3))
+                    
+                ),
+                Bar(
+                    name = 'Not classified in class',
+                    y=top_classes['categories'],
+                    x=top_classes['false_proportion -0'],
+                    orientation = 'h',
+                    marker=dict(
+                            color='rgba(58, 71, 80, 0.6)',
+                            line=dict(color='rgba(58, 71, 80, 1.0)', width=3)
+    )
+                )
+            ],
+            'layout':{
+                'barmode' : 'stack',
+                'title': 'Top 5 message categories',
+                "xaxis": {
+                    'title': 'Number of messages'
+                },
+                "yaxis": {
+                    'title': 'Categories',
+                    'title_standoff' : 40, 
+                    'tickangle' : 45
+                },
+               # 'template': "seaborn"
+            }
+
+        },
+       {
             'data': [
                 Bar(
                     x=genre_names,
                     y=genre_counts
                 )
             ],
-
             'layout': {
                 'title': 'Distribution of Message Genres',
                 'yaxis': {
@@ -76,20 +121,21 @@ def index():
                 },
                 'xaxis': {
                     'title': "Genre"
-                }
+                },
+                'template': "seaborn"
             }
-        }
+        },
     ]
     
-    # encode plotly graphs in JSON
+    #   encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
     
-    # render web page with plotly graphs
+    #   render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
 
-# web page that handles user query and displays model results
+#   web page that handles user query and displays model results
 @app.route('/go')
 def go():
     # save user input in query
